e12219ca86f7c09ff6d32425ddca1a9bc63ab157
Carlo <CarloMehegan@gmail.com>
2022-04-11 17:55:34 -0400

    script for wordcloud of genres
    creates wordcloud of genres from the 9 border countries

---
+++ genre wordcloud.py
@@ -0,0 +1,36 @@
+# CS100 final project
+# Spotify@Russia
+
+countries = ['Belarus', 'Estonia', 'Finland', 'Kazakhstan', 'Latvia', 'Lithuania', 'Norway', 'Poland', 'Ukraine']
+other = ['Global', 'USA', 'Russia'] #Russia playlist is now unavailable!
+start_date = 40822
+day_increment = 100
+end_date = 41122
+
+import pandas as pd
+genres = ""
+for c in countries:
+    for i in range(start_date, end_date+1, day_increment):
+        file_name = ("0" + str(i) + " " + str(c) + ".csv") # format: "040822 Belarus.csv"
+        try:
+            data = pd.read_csv(file_name, dtype="string")
+            s = ""
+            for i in data.Genres.values:
+                if str(i) == "<NA>":
+                    pass
+                else:
+                    word = i.replace(" ", "")
+                    s = s + word + "\n"
+            genres = genres + "\n" + s
+        except:
+            print("file not found:", file_name)
+
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+wordcloud = WordCloud(collocations = False).generate(genres)
+plt.figure(figsize=[10,10])
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
